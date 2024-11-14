9c77c0ed68cbc2f159889c7c46364f3058ad0420
Sam Colson <samcolson4@gmail.com>
2024-11-14 05:26:23 +0000

    Add show route to controller

--- app/controllers/api/v1/effects_unit_controller.rb
+++ app/controllers/api/v1/effects_unit_controller.rb
@@ -4,6 +4,14 @@ module Api
       def index
         render jsonapi: EffectUnit.all, class: { EffectUnit: SerializableEffectUnit }
       end
+
+      def show
+        effect_unit = EffectUnit.find(params['id'])
+
+        if effect_unit.present?
+          render jsonapi: effect_unit, class: { EffectUnit: SerializableEffectUnit }
+        end
+      end
     end
   end
 end
--- test/controllers/api/v1/effects_unit_controller_test.rb
+++ test/controllers/api/v1/effects_unit_controller_test.rb
@@ -18,7 +18,28 @@ class EffectsUnitControllerTest < ActionDispatch::IntegrationTest
 
     unit_b_json = json_response['data'].last['attributes']
     assert_equal unit_b.name, unit_b_json['name']
-    assert_equal unit_b.main_image_link, unit_b_json['main_image_link']
+    assert_nil unit_b.main_image_link, unit_b_json['main_image_link']
     assert_equal unit_b.format, unit_b_json['format']
+  end
+
+  test '#show - gets a single effect unit' do
+    unit = create(:effect_unit)
+
+    get api_v1_effects_unit_url(id: unit.id)
+
+    assert_response :success
+
+    json_response = JSON.parse(response.parsed_body)
+
+    unit_json = json_response['data']['attributes']
+    assert_equal unit.name, unit_json['name']
+    assert_equal unit.main_image_link, unit_json['main_image_link']
+    assert_equal unit.format, unit_json['format']
+  end
+
+  test '#show - returns a 404 if no effect unit by id is found' do
+    get api_v1_effects_unit_url(id: 1234)
+
+    assert_response :not_found
   end
 end
